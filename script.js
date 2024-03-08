b95db9e9119b8c59d2e8925f387b38a09f0f649b
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-08 12:42:18 +0600

    add new button

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
-function handleSelect(){
-    console.log('boss ok');
+
+
+const allBtn = document.getElementsByClassName('add-btn');
+
+let count = 0;
+
+for(const btn of allBtn){
+    btn.addEventListener('click', function (e){
+        count = count + 1;
+        document.getElementById('card-count').innerText = count;
+    });
 }
